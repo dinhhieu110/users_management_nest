2fe0167d3ef6a01d365508d76840030de2673772
dinhhieu110 <trandinhhieu11002@gmail.com>
2025-03-16 17:27:52 +0700

    [UMN-5] Add Throttler to limit API calls

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 // Entry file
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn'],
+  });
+  app.enableCors();
+  app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
